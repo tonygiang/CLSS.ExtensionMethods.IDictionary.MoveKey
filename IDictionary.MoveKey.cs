7ea0737c5177c161765b6cf1e0dfea1cc16d1676
tonygiang <tony_s_giang@yahoo.com>
2022-07-27 14:23:45 +0700

    Project push.

---
+++ IDictionary.MoveKey.cs
@@ -0,0 +1,117 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System.Collections.Generic;
+using System;
+
+namespace CLSS
+{
+  public static partial class IDictionaryMoveKey
+  {
+    /// <summary>
+    /// Moves the value of an existing key to the new key if the new key did not
+    /// already exist.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.
+    /// </typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.
+    /// </typeparam>
+    /// <param name="dictionary">A dictionary with keys of type
+    /// <typeparamref name="TKey"/> and values of type
+    /// <typeparamref name="TValue"/>.</param>
+    /// <param name="existingKey">The key of the value to move from.</param>
+    /// <param name="newKey">The key of the value to move to.</param>
+    /// <returns>The source dictionary.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dictionary"/> is
+    /// null.</exception>
+    /// <exception cref="ArgumentException">An element with newKey already
+    /// exists in the <see cref="IDictionary{TKey, TValue}"/></exception>
+    public static IDictionary<TKey, TValue> MoveKey<TKey, TValue>(
+      this IDictionary<TKey, TValue> dictionary,
+      TKey existingKey,
+      TKey newKey)
+    {
+      if (dictionary == null) throw new ArgumentNullException("source");
+      if (dictionary.ContainsKey(newKey))
+        throw new ArgumentException("The new key to move value to is not available.");
+      var value = dictionary[existingKey];
+      dictionary.Remove(existingKey);
+      dictionary[newKey] = value;
+      return dictionary;
+    }
+
+    /// <summary>
+    /// Moves the value of an existing key to the new key if the new key did not
+    /// already exist.
+    /// </summary>
+    /// <typeparam name="T">The type of
+    /// <see cref="IDictionary{TKey, TValue}"/> to move key.</typeparam>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.
+    /// </typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.
+    /// </typeparam>
+    /// <param name="dictionary">A dictionary with keys of type
+    /// <typeparamref name="TKey"/> and values of type
+    /// <typeparamref name="TValue"/>.</param>
+    /// <param name="existingKey">The key of the value to move from.</param>
+    /// <param name="newKey">The key of the value to move to.</param>
+    /// <returns>The source dictionary.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dictionary"/> is
+    /// null.</exception>
+    /// <exception cref="ArgumentException">An element with newKey already
+    /// exists in the <see cref="IDictionary{TKey, TValue}"/></exception>
+    public static T MoveKey<T, TKey, TValue>(this T dictionary,
+      TKey existingKey,
+      TKey newKey)
+      where T : IDictionary<TKey, TValue>
+    {
+      if (dictionary == null) throw new ArgumentNullException("source");
+      if (dictionary.ContainsKey(newKey))
+        throw new ArgumentException("The new key to move value to is not available.");
+      var value = dictionary[existingKey];
+      dictionary.Remove(existingKey);
+      dictionary[newKey] = value;
+      return dictionary;
+    }
+
+    /// <summary>
+    /// Moves the value of an existing key to the new key. If the
+    /// <see cref="IDictionary{TKey, TValue}"/> already contains an element with
+    /// <paramref name="newKey"/>, that value is assigned to the out argument
+    /// <paramref name="conflictingValue"/>.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.
+    /// </typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.
+    /// </typeparam>
+    /// <param name="dictionary">A dictionary with keys of type
+    /// <typeparamref name="TKey"/> and values of type
+    /// <typeparamref name="TValue"/>.</param>
+    /// <param name="existingKey">The key of the value to move from.</param>
+    /// <param name="newKey">The key of the value to move to.</param>
+    /// <param name="conflictingValue">When this method returns, the value
+    /// previously associated with <paramref name="newKey"/>, if
+    /// <paramref name="newKey"/> existed; otherwise, the default value for the
+    /// type of the value parameter. This parameter is passed uninitialized.
+    /// </param>
+    /// <returns>true if the object that implements
+    /// <see cref="IDictionary{TKey, TValue}"/> contains an element with
+    /// <paramref name="newKey"/>; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dictionary"/> is
+    /// null.</exception>
+    public static bool TryMoveKey<TKey, TValue>(
+      this IDictionary<TKey, TValue> dictionary,
+      TKey existingKey,
+      TKey newKey,
+      out TValue conflictingValue)
+    {
+      if (dictionary == null) throw new ArgumentNullException("source");
+      conflictingValue = default(TValue);
+      var hasConflict = dictionary.ContainsKey(newKey);
+      if (hasConflict) conflictingValue = dictionary[existingKey];
+      var value = dictionary[existingKey];
+      dictionary.Remove(existingKey);
+      dictionary[newKey] = value;
+      return hasConflict;
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,51 @@
+﻿# CLSS.ExtensionMethods.IDictionary.MoveKey
+
+### Problem
+
+Changing the key of a key/value pair in any `IDictionary` collection is a 3-line process by default:
+
+```
+var value = dict[existingKey];
+dict.Remove(existingKey);
+dict[newKey] = value;
+```
+
+Checking for conflicting keys requires even more lines of code.
+
+### Solution
+
+`MoveKey` does this in 1 line:
+
+```
+using CLSS;
+
+dict.MoveKey(existingKey, newKey);
+```
+
+In case the source `IDictionary` contains an element with `newKey`, `MoveKey` will throw an exception and the key move will not proceed.
+
+With an implicit type invocation, it returns an `IDictionary<TKey, TValue>`. With an explicit type invocation, it returns the original collection type.
+
+```
+using CLSS;
+
+var customSortedNames = new Dictionary<int, string>()
+{
+  [1] = "Ryan",
+  [2] = "David",
+  [3] = "Wendell"
+};
+numbers.MoveKey(2, 5); // returns IDictionary<int, string>
+numbers.MoveKey<Dictionary<int, string>, int, string>(1, 6); // returns Dictionary<int, string>;
+```
+
+This package also provides the `TryMoveKey` method. In the case of new key conflict, `TryMoveKey` will proceed, assign the conflicting value that was previously associated to `newKey` to the 3rd argument and return true. `TryMoveKey` returns false if there is no conflict.
+
+```
+using CLSS;
+
+if (dict.TryMoveKey(existingKey, newKey, out var conflictingValue))
+{ /* Do something with conflictingValue */ }
+```
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
